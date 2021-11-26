1bb48b8a90ee4dbfcf46b7fa20533aa9cfef32a2
tysonmedora6 <44410848+tysonmedora6@users.noreply.github.com>
2021-11-26 19:15:24 +0530

    Delete sample.js

--- sample.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const getData = async function () {
-Promise.all(
-     fetch('https://api.chucknorris.io/jokes/random').then((resp) => resp.json()),
-  );
-}
-
-export default getData;
